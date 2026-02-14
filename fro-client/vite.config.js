acfefb2b944badfb388182f7b440d1748ea65d8b
MungangaThelly <daddymulongo@yahoo.fr>
2026-02-14 18:51:26 +0100

    "Prepare for Vercel deployment"

--- fro-client/vite.config.js
+++ fro-client/vite.config.js
@@ -1,12 +1,10 @@
-// vite.config.js  
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3001" // Proxiar API-anrop till Express
-    }
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
-});
+})
